6a0858eb2aa3c230b809dc6c40bde7623733054f
kongrp <960139253@qq.com>
2016-09-28 16:20:55 +0800

    3.2.2 修改数据库连接配置

--- application/database.php
+++ application/database.php
@@ -15,7 +15,7 @@ return [
     // 服务器地址
     'hostname'       => '127.0.0.1',
     // 数据库名
-    'database'       => '',
+    'database'       => 'tp5.0',
     // 用户名
     'username'       => 'root',
     // 密码
@@ -29,9 +29,9 @@ return [
     // 数据库编码默认采用utf8
     'charset'        => 'utf8',
     // 数据库表前缀
-    'prefix'         => '',
+    'prefix'         => 'yunzhi_',
     // 数据库调试模式
-    'debug'          => true,
+    'debug'          => APP_DEBUG,
     // 数据库部署方式:0 集中式(单一服务器),1 分布式(主从服务器)
     'deploy'         => 0,
     // 数据库读写是否分离 主从式有效
