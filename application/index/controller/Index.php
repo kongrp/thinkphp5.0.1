ea134d79703418cfc5bb466919abecb0423aa4cb
kongrp <960139253@qq.com>
2016-09-28 17:22:47 +0800

    3.2.3 直接读取数据

--- application/index/controller/Index.php
+++ application/index/controller/Index.php
@@ -1,10 +1,11 @@
 <?php
-namespace app\index\controller;
+namespace app\index\controller;   //命名空间
+use think\Db;  //引用数据库操作类
 
 class Index
 {
     public function index()
     {
-        return 'Hello World';
+        var_dump(Db::name('teacher')->find());   //获取数据表中的第一条数据
     }
 }
